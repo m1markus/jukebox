415f6e1b513fd161ef3f0116c41ff02a727ff7af
mmm <mmm7@bluewin.ch>
2021-09-19 12:44:30 +0200

    Initial app and server

---
+++ app-android/app/src/main/java/com/books/webview/MainActivity.java
@@ -0,0 +1,97 @@
+package com.books.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    ProgressBar progressBar;
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    // for local development
+    //String url = "http://dellxps13-1:8080/jukebox.html";
+    String url = "http://m1m-macbook:8080/jukebox.html";
+    //
+    // just for playing around
+    //String url = "https://appassets.androidplatform.net/assets/index.html";
+    //String url = "https://www.google.com";
+    //
+    // for worldwide usage
+    //String url = "http://m1m.ch/jukebox/jukebox.html"
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        webView = findViewById(R.id.web);
+        progressBar = findViewById(R.id.progress);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        MyWebViewclient myWebViewclient = new MyWebViewclient();
+        webView.setWebViewClient(myWebViewclient);
+        webView.loadUrl(url);
+    }
+
+    public class MyWebViewclient extends WebViewClient{
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Toast.makeText(getApplicationContext(), "No internet connection", Toast.LENGTH_LONG).show();
+            webView.loadUrl("file:///android_asset/lost.html");
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            super.onReceivedSslError(view, handler, error);
+            handler.cancel();
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ app-server/src/test/java/ch/m1m/jukebox/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package ch.m1m.jukebox;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
