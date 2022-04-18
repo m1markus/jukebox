1309fe3a15ab79f97a6f6d1afd1c4b77af7daee0
mmm <mmm7@bluewin.ch>
2022-04-18 11:23:27 +0200

    Add lifecycle bean

---
+++ app-server/src/main/java/ch/m1m/jukebox/AppLifecycleBean.java
@@ -0,0 +1,29 @@
+package ch.m1m.jukebox;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import ch.m1m.jukebox.model.db.Song;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class AppLifecycleBean {
+
+    @Inject
+    SongService songService;
+
+    private static final Logger LOG = Logger.getLogger("ListenerBean");
+
+    void onStart(@Observes StartupEvent ev) {
+        LOG.info("The application is starting...");
+
+        //songService.insert(new Song("10", "mc marc", "do it"));
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        LOG.info("The application is stopping...");
+    }
+}
